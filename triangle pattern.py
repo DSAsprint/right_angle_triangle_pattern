3987e98bb38114d8174939b7cae2efb498188c73
Salome Mwaka Chipa <97435779+Sarleymwaka@users.noreply.github.com>
2023-08-18 05:42:47 +0300

    Update triangle pattern.py

--- triangle pattern.py
+++ triangle pattern.py
@@ -1,6 +1,6 @@
 def right_angle_triangle(n):
     for i in range(1, n + 1):
-        print('* ' * n - i * i)
+        print('* ' * i)
 
 n = int(input("Enter the number of rows: "))
 right_angle_triangle(n)
